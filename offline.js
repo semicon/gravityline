7257e00f919c89b3382e18504c2adce4ece7dfd0
semicon <30399464+semicon@users.noreply.github.com>
2019-07-04 20:33:24 +0700

    gravityline
    gravityline

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1562247089,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/base-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/dot1-sheet0.png",
+		"images/dot2-sheet0.png",
+		"images/line-sheet0.png",
+		"images/goalintro-sheet0.png",
+		"images/bg.png",
+		"images/goal-sheet0.png",
+		"images/goal-sheet1.png",
+		"images/goal-sheet2.png",
+		"images/bruch-sheet0.png",
+		"images/box-sheet0.png",
+		"images/checkpoint-sheet0.png",
+		"images/head-sheet0.png",
+		"images/line2-sheet0.png",
+		"images/compleat.png",
+		"images/replay-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
